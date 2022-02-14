8b009b707922627fa78983263aa6c5085c92dfbe
eroonkang <erooneroon@gmail.com>
2022-02-13 21:53:31 -1000

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$("#open").click(function(){
+  $(this).hide();
+  $("#close").show();
+  $("#target").slideDown('fast');
+});
+$("#close").click(function(){
+  $("#target").hide();
+  $("#close").hide();
+  $("#open").show();
+});
